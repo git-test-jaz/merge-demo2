31098a1c0e7e415e82c7200cf6811675f46f385b
Git Demos <tutcollection@gmail.COM>
2018-07-17 13:08:00 +0500

    main component added

---
+++ main-comp.php
@@ -0,0 +1,15 @@
+<?
+echo "footer line 1";
+echo "Added feature 2 by Developer 1";
+
+//CHanges added by developer yyyy
+//Code goes here
+//developer 2 changes ending here
+
+/*More changes by developer xxxx
+//Some code goes here
+/*Changes ending here*/
+
+?>
+
+
